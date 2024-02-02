2a659d6d215098ec8412d64010bf04a693d91ffc
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-02 20:22:59 +0300

    Fix layout

--- PersonalityQuiz/StartQuizViewController.swift
+++ PersonalityQuiz/StartQuizViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartQuizViewController.swift
 //  PersonalityQuiz
 //
 //  Created by Александр Плешаков on 02.02.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartQuizViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
