b56727df7710443abe18c00a397275882217f316
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-02 19:10:30 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Александр Плешаков on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
