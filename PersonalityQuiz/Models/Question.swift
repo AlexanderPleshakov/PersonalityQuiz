e00e52584a1fd33d1f23342965bee5483fc94726
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-03 15:38:59 +0300

    Added the question and answer models

---
+++ PersonalityQuiz/Models/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by Александр Плешаков on 03.02.2024.
+//
+
+import Foundation
+
+struct Question {
+    var text: String
+    var type: ResponseType
+    var answers: [Answer]
+}
+
+enum ResponseType {
+    case single, multiply, ranged
+}
