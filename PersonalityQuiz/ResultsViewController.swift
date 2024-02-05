b613cc97cbb1fce7bacb273e84199f097b3e9788
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-05 10:45:55 +0300

    Added inits for result controller

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 final class ResultsViewController: UIViewController {
+    
+    // MARK: Properties
+    var responses: [Answer]
+    
+    // MARK: Init
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Outlets
+    
+    
+    // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
+    
+    // MARK: Methods
+    
+    
+    
+    // MARK: Actions
+    
+    
 }
