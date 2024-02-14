54855cc9694a13339c0e17e15f32a9caae00297b
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-02-14 08:18:48 +0300

    Added the display of quizzes

--- TechnicalQuiz/QuizzesViewController.swift
+++ TechnicalQuiz/QuizzesViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 final class QuizzesViewController: UIViewController {
+    // MARK: Properties
+    let allQuizzes = ["Linux", "BASH", "PHP", "Docker", "HTML", "MySQL", "WordPress", "Laravel", "Kubernetes", "JavaScript", "DecOps"]
     
     // MARK: Outlets
     
@@ -25,3 +27,23 @@ final class QuizzesViewController: UIViewController {
         
     }
 }
+
+// MARK: UITableViewDataSource
+
+extension QuizzesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allQuizzes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = allQuizzes[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
