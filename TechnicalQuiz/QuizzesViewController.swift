c9d4a977963fb7ca54026eeeca3fecf61d57d967
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-02-14 07:56:06 +0300

    Added the quizzes table view

---
+++ TechnicalQuiz/QuizzesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  QuizzesViewController.swift
+//  TechnicalQuiz
+//
+//  Created by Александр Плешаков on 14.02.2024.
+//
+
+import UIKit
+
+final class QuizzesViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var QuizzesTableView: UITableView!
+    
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Methods
+    
+    private func configure() {
+        
+    }
+}
