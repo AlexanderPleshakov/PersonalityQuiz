b3e120e8db7c171cb7a4260f810d46eeab8e7a36
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-02-24 14:02:54 +0300

    Added questions loader

--- TechnicalQuiz/QuizAPI/QuestionsLoader.swift
+++ TechnicalQuiz/QuizAPI/QuestionsLoader.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+final class QuestionsLoader {
+    
+    func loadQuestions(handler: @escaping (Result<Questions, Error>) -> Void) {
+        NetworkManager.shared.fetch(url: Link.bash.url) { result in
+            switch result {
+            case .success(let data):
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                do {
+                    let questions = try jsonDecoder.decode(Questions.self, from: data)
+                    handler(.success(questions))
+                } catch {
+                    handler(.failure(error))
+                }
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+    }
+}
